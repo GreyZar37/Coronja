7c73532474f4ffae053fea330cf068ec23312f96
MeisterRepsak <71341417+MeisterRepsak@users.noreply.github.com>
2020-09-25 22:02:55 +0200

    t

---
+++ Coronja/Assets/Scripts/EnemyLook.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLook : MonoBehaviour
+{
+    private GameObject Player;
+
+    public float Speed;
+    [Range(0, 1)] [SerializeField] private float MovementSmoothing;
+    Vector3 Velocity = Vector3.zero;
+
+    private Rigidbody2D rb2D;
+    void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        LookAtPlayer();
+        MoveToPlayer();
+    }
+
+    void LookAtPlayer()
+    {
+        Vector2 target = new Vector2(Player.transform.position.x, Player.transform.position.y);
+
+        Vector2 direction = new Vector2(target.x - transform.position.x, target.y - transform.position.y);
+
+        transform.up = direction;
+    }
+
+    void MoveToPlayer()
+    {
+       Vector3 Targetvelocity = transform.up * Speed * 10;
+
+        rb2D.velocity = Vector3.SmoothDamp(rb2D.velocity, Targetvelocity, ref Velocity, MovementSmoothing) * Time.fixedDeltaTime;
+    }
+}
---
+++ Coronja/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //components
+    private Rigidbody2D rb2D;
+   
+    //variabler
+    Vector3 Velocity = Vector3.zero;
+    public float Speed;
+    [Range(0, 1)] [SerializeField] private float MovementSmoothing;
+   
+    void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+
+    }
+    void FixedUpdate()
+    {
+        float xAxis = Input.GetAxisRaw("Horizontal");
+        float yAxis = Input.GetAxisRaw("Vertical");
+
+        
+
+
+        Vector3 Targetvelocity = new Vector3 ( xAxis, yAxis, 0).normalized;
+
+        rb2D.velocity = Vector3.SmoothDamp(rb2D.velocity, (Targetvelocity * 10) * Speed , ref Velocity, MovementSmoothing) * Time.fixedDeltaTime;
+    }
+        /*
+        if (move > 0 && FacingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && !FacingRight)
+        {
+            Flip();
+        }
+
+    }
+    void Flip()
+    {
+        FacingRight = !FacingRight;
+
+        Vector3 TheScale = transform.localScale;
+
+        TheScale.x *= -1;
+
+        transform.localScale = TheScale;
+    }
+    */
+}
+
