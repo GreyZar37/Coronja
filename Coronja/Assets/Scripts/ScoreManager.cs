5d24b3c7e719a61c58e53d941022289b81264d42
GreyZar37 <52458016+GreyZar37@users.noreply.github.com>
2020-09-26 14:34:27 +0200

    g

---
+++ Coronja/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;
+
+    private TextMeshPro TmproText;
+    
+    void Awake()
+    {
+        TmproText = GetComponent<TextMeshPro>();
+        score = 0;
+    }
+    void update ()
+    {
+        TmproText.text = "Score: " + score;
+    }
+
+
+}
